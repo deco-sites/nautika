5a8d60dfa013526c5113e4c623a019c5481e3312
Guilherme Heringer <guilhermeheringer@airdeguilherme.lan>
2023-11-22 16:53:40 +0000

    “adjust”

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,15 @@ export default {
   plugins: [daisyui],
   daisyui: { themes: [], logs: false },
   content: ["./**/*.tsx"],
-  theme: { container: { center: true } },
+  theme: { 
+    container: { center: true },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'yellow' : '#ff9800',
+    },
+  },
+  
 };
